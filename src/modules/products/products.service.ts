dd11e7310c2be64fa78ed095143926a50c3901f3
Juan David Garcia <juancho637@gmail.com>
2023-08-09 15:00:32 -0500

    fix: change find all message (#2)
    Co-authored-by: Juan David Garcia <>

--- src/modules/products/products.service.ts
+++ src/modules/products/products.service.ts
@@ -9,7 +9,7 @@ export class ProductsService {
   }
 
   findAll() {
-    return `This action returns all products`;
+    return `This action returns all products hehe`;
   }
 
   findOne(id: number) {
